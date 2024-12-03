f23d5f378b6dc9fd170f68455ec29ca0675ce790
Krishna Kant <kantk2492@gmail.com>
2024-12-03 10:52:59 +0530

    fv

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,39 @@
             <div>fad</div>
             <nav class="mt-2">
                 <ul>
-                    <li class="nav-item">SF</li>
+                    <li class="nav-item">hello
+                      <a href="#" class="nav-link"></a>
+                      <ul class="nav nav-treeview" style="display:block;">
+                            <li class="nav-item menu-is-opening menu-open">
+                                <a href="" class="nav-link">
+                                  <i class="nav-icon far fa-plus-square"></i>
+                                  <p>Invoice
+                                    <i class="far fa-angle-left right"></i>
+                                  </p>
+                                </a>
+                            </li>
+                            <liclass="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <liclass="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <liclass="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <liclass="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                        </ul>
+                    </li>
                     <li class="nav-item">SF</li>
                     <li class="nav-item">SF</li>
                     <li class="nav-item">SF</li>
